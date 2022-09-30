b00dcabe00340573efdad0263730cabbd2f74c5c
Malhabib <37366964+Malhabib@users.noreply.github.com>
2022-09-30 22:25:51 +0300

    Add files via upload

--- src/app.py
+++ src/app.py
@@ -1,118 +1,79 @@
-from fastai import *
-from fastai.vision import *
-import fastai
-import yaml
-import sys
-from io import BytesIO
-from typing import List, Dict, Union, ByteString, Any
-import os
-import flask
-from flask import Flask
-import requests
-import torch
-import json
+import tensorflow as tf
+model = tf.keras.models.load_model('Farida_food_classifier.hdf5')
+import streamlit as st
+st.write("""
+         # Mohammed FOOD classifier for Food101
+         """
+         )
 
-with open("src/config.yaml", 'r') as stream:
-    APP_CONFIG = yaml.full_load(stream)
-
-app = Flask(__name__)
+file = st.file_uploader("Upload Your Image", type=["jpg", "png"])
 
 
-def load_model(path=".", model_name="model.pkl"):
-    learn = load_learner(path, fname=model_name)
-    return learn
 
-
-def load_image_url(url: str) -> Image:
-    response = requests.get(url)
-    img = open_image(BytesIO(response.content))
-    return img
-
-
-def load_image_bytes(raw_bytes: ByteString) -> Image:
-    img = open_image(BytesIO(raw_bytes))
-    return img
-
-
-def predict(img, n: int = 3) -> Dict[str, Union[str, List]]:
-    pred_class, pred_idx, outputs = model.predict(img)
-    pred_probs = outputs / sum(outputs)
-    pred_probs = pred_probs.tolist()
-    predictions = []
-    for image_class, output, prob in zip(model.data.classes, outputs.tolist(), pred_probs):
-        output = round(output, 1)
-        prob = round(prob, 2)
-        predictions.append(
-            {"class": image_class.replace("_", " "), "output": output, "prob": prob}
-        )
-
-    predictions = sorted(predictions, key=lambda x: x["output"], reverse=True)
-    predictions = predictions[0:n]
-    return {"class": str(pred_class), "predictions": predictions}
-
-
-@app.route('/api/classify', methods=['POST', 'GET'])
-def upload_file():
-    if flask.request.method == 'GET':
-        url = flask.request.args.get("url")
-        img = load_image_url(url)
+from PIL import Image, ImageOps
+import numpy as np
+def import_and_predict(image_data, model):
+    
+        size = (256,256)    
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = np.asarray(image)
+        img_resize = image/255.
+        
+        img_reshape = img_resize[np.newaxis,...]
+    
+        prediction = model.predict(img_reshape)
+        
+        return prediction
+        
+if file is None:
+    st.text("No File, Please upload an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    
+    if np.argmax(prediction) == 0:
+        st.write("It is a apple_pie!")
+    elif np.argmax(prediction) == 1:
+        st.write("It is a baby_black_ribs!")
+    elif np.argmax(prediction) == 2:
+        st.write("It is a baklava!")
+    elif np.argmax(prediction) == 3:
+        st.write("It is a beef_carpaccio!")
+    elif np.argmax(prediction) == 4:
+        st.write("It is a beef_tartare!")
+    elif np.argmax(prediction) == 5:
+        st.write("It is a beet_salad!")
+    elif np.argmax(prediction) == 6:
+        st.write("It is a beignets!")
+    elif np.argmax(prediction) == 7:
+        st.write("It is a bibimbap!")
+    elif np.argmax(prediction) == 8:
+        st.write("It is a bread_pudding!")
+    elif np.argmax(prediction) == 9:
+        st.write("It is a breakfast_burrito!")
+    elif np.argmax(prediction) == 10:
+        st.write("It is a bruschetta!")
+    elif np.argmax(prediction) == 11:
+        st.write("It is a caesar_salad!")
+    elif np.argmax(prediction) == 12:
+        st.write("It is a cannoli!")
+    elif np.argmax(prediction) == 13:
+        st.write("It is a caprese_salad!")
+    elif np.argmax(prediction) == 14:
+        st.write("It is a carrot_cake!")
+    elif np.argmax(prediction) == 15:
+        st.write("It is a ceviche!")
+    elif np.argmax(prediction) == 16:
+        st.write("It is a cheesecake!")
+    elif np.argmax(prediction) == 17:
+        st.write("It is a cheese_plate!")
+    elif np.argmax(prediction) == 18:
+        st.write("It is a chicken_curry!")
+    elif np.argmax(prediction) == 19:
+        st.write("It is a chicken_curry!")
     else:
-        bytes = flask.request.files['file'].read()
-        img = load_image_bytes(bytes)
-    res = predict(img)
-    return flask.jsonify(res)
-
-
-@app.route('/api/classes', methods=['GET'])
-def classes():
-    classes = sorted(model.data.classes)
-    return flask.jsonify(classes)
-
-
-@app.route('/ping', methods=['GET'])
-def ping():
-    return "pong"
-
-
-@app.route('/config')
-def config():
-    return flask.jsonify(APP_CONFIG)
-
-
-@app.after_request
-def add_header(response):
-    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
-    response.headers["Expires"] = 0
-    response.headers["Pragma"] = "no-cache"
-
-    response.cache_control.max_age = 0
-    return response
-
-
-@app.route('/<path:path>')
-def static_file(path):
-    if ".js" in path or ".css" in path:
-        return app.send_static_file(path)
-    else:
-        return app.send_static_file('index.html')
-
-
-@app.route('/')
-def root():
-    return app.send_static_file('index.html')
-
-
-def before_request():
-    app.jinja_env.cache = {}
-
-
-model = load_model('models')
-
-if __name__ == '__main__':
-    port = os.environ.get('PORT', 5000)
-
-    if "prepare" not in sys.argv:
-        app.jinja_env.auto_reload = True
-        app.config['TEMPLATES_AUTO_RELOAD'] = True
-        app.run(debug=False, host='0.0.0.0', port=port)
-        # app.run(host='0.0.0.0', port=port)
+        st.write("Other")
+    
+    st.text("Probability (0: 'apple_pie', 1: 'baby_back_ribs', 2:'baklava', 3:'beef_carpaccio',4:'beef_tartare', 5: 'beet_salad', 6:'beignets', 7:'bibimbap',8: 'bread_pudding', 9:'breakfast_burrito', 'bruschetta', 'caesar_salad', 'cannoli', 'caprese_salad', 'carrot_cake', 'ceviche', 'cheesecake','cheese_plate', 'chicken_curry', 'chicken_quesadilla', 'big_class' ")
+    st.write(prediction)
